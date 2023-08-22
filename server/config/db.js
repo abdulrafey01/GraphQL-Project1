d0034c38f6f86aeac86a34f05a30ff0c598575dc
Abdul Rafey <lights.of.life11@gmail.com>
2023-08-22 03:51:52 -0700

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const mongoUri = process.env.MONGO_URI
 
 const connectToMongoose =  async ()=>{
-    mongoose.connect("mongodb+srv://AbdulRafey:LightsOfLife@cluster0.ayls3gp.mongodb.net/mgmt_db?retryWrites=true&w=majority")
+    mongoose.connect("Your DB URL")
     .then(() => {
        console.log("Database Connected")
     })
